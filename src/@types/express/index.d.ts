24a760e0483c973f331d9cc2213c83e4761c8329
ali-grn <aligarajian1@gmail.com>
2021-08-11 23:41:28 +0430

    chore: init

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,20 @@
+import { IUser } from '@entities/User';
+import { IClientData } from '@shared/JwtService';
+
+declare module 'express' {
+  export interface Request {
+    body: {
+      user: IUser;
+      email: string;
+      password: string;
+    };
+  }
+}
+
+declare global {
+  namespace Express {
+    export interface Response {
+      sessionUser: IClientData;
+    }
+  }
+}
